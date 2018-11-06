4ac14033b617fd28b4dc6d96040a3af8bac3b3b2
dan <user1@client-1.local>
2018-11-06 10:09:57 +0000

    new tree, part2 added

---
+++ 02Basics/Example/01GettingStarted/01HellowWorldPart2.py
@@ -0,0 +1,8 @@
+# Name      : 01HelloWorldPart2
+# Author    : Dan Thomas
+# Date      : 06 Nov 2018
+# Purpose   : Example of a simple output
+
+print("Hello World")
+print('Hello World')
+print("Wayne's World")
